b01f794828ea20794eeaee2a2d61b7de14b5533b
Vanessa Glowczewski <vglowczewski@bayviewglen.ca>
2023-11-30 17:40:39 -0500

    vue

---
+++ vue3-intro/creating-vue-app/main.js
@@ -0,0 +1,11 @@
+// const product = 'Socks'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            price: 5.99,
+            description: 'Nice and warm blue socks. Great for a gift for someone who has cold feet.'
+        }
+    }
+})
---
+++ vue3-intro/list-rendering/main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+   data() {
+      return {
+         product: 'Socks',
+         image: './assets/images/socks_blue.jpg',
+         inStock: true,
+         details: ['50% cotton', '30% wool', '20% polyester'],
+         sizes: ['XS', 'S', 'M', 'L', 'XL']
+      }
+   }
+})
