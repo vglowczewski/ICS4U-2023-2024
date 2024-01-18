f0f5d7694194a812878a1bbb190a340ea127b009
Vanessa Glowczewski <vglowczewski@bayviewglen.ca>
2024-01-18 09:47:24 -0500

    test

---
+++ Movie_API/main.js
@@ -0,0 +1,17 @@
+const url = 'https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0a87b362f6msh2f6e05eb0173132p14dfebjsna97d373936a9',
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
---
+++ travel-app/vue-school-travel-app/src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+  </div>
+</template>
+
+
+<style>
+</style>
