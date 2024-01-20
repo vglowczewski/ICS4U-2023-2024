8fc66a5561b1b8d1fb61f1f43347622df9684182
Vanessa Glowczewski <vglowczewski@bayviewglen.ca>
2024-01-20 16:13:42 -0500

    added nav

--- travel-app/vue-school-travel-app/src/router/index.js
+++ travel-app/vue-school-travel-app/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import Home from '@/views/HomeView.vue'
+import About from '@/views/AboutView.vue'
+import Brazil from '@/views/Brazil.vue'
+import Hawaii from '@/views/Hawaii.vue'
+import Jamaica from '@/views/Jamaica.vue'
+import Panama from '@/views/Panama.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +12,32 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
       path: '/about',
       name: 'about',
-      component: AboutView
+      component: About
+    },
+    {
+      path: '/brazil',
+      name: 'brazil',
+      component: Brazil
+    },
+    {
+      path: '/hawaii',
+      name: 'hawaii',
+      component: Hawaii
+    },
+    {
+      path: '/jamaica',
+      name: 'jamaica',
+      component: Jamaica
+    },
+    {
+      path: '/panama',
+      name: 'panama',
+      component: Panama
     }
   ]
 })
