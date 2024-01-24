80c8ceea86f5be64f9afc1ad868a0247667f9b4c
Vanessa Glowczewski <vglowczewski@bayviewglen.ca>
2024-01-24 17:41:00 -0500

    end of lesson 2

--- travel-app/vue-school-travel-app/src/router/index.js
+++ travel-app/vue-school-travel-app/src/router/index.js
@@ -13,8 +13,16 @@ const router = createRouter({
       path: '/destination/:id/:slug',
       name: 'destination.show',
       component: ()=> import('@/views/DestinationShow.vue'),
-      props: route=>({id: parseInt(route.params.id)}),
-    }
+      props: route=>({...route.params, id: parseInt(route.params.id)}),
+      children:[
+        {
+          path: ':experienceSlug',
+          name: 'experience.show',
+          component: ()=> import('@/views/ExperienceShow.vue'),
+          props: route=>({...route.params, id: parseInt(route.params.id)}),
+        }
+      ]
+    },
   ]
 })
 
