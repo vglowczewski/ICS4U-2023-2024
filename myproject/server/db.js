06c7c994560a8b9776982437f3a542a00974ed7f
Vanessa Glowczewski <vglowczewski@bayviewglen.ca>
2024-02-15 10:34:35 -0500

    fixed

--- myproject/server/db.js
+++ myproject/server/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+// Load environment variables from .env file
+require('dotenv').config();
 
 const connectionString = `mongodb+srv://vanvalglow:<homecheck>@cluster0.wnqqa4d.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(connectionString, {
+// Connect to MongoDB using the URI from the environment variable
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log('Database connected')).catch((err) => console.log(err));
+})
+.then(() => console.log('Database connected'))
+.catch((err) => console.error('Database connection error:', err));
